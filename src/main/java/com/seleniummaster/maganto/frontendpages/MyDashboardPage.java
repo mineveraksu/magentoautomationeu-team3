22cd09070b521523d7e27f7ac936610ee80b9101
Matros <guzalhayat84@gmail.com>
2022-06-29 11:50:33 +0300

    dashboard

--- src/main/java/com/seleniummaster/maganto/frontendpages/MyDashboardPage.java
+++ src/main/java/com/seleniummaster/maganto/frontendpages/MyDashboardPage.java
@@ -1,4 +1,33 @@
 package com.seleniummaster.maganto.frontendpages;
 
+import com.seleniummaster.maganto.utility.TestUtility;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class MyDashboardPage {
+    WebDriver driver;
+    TestUtility testUtility;
+
+    public MyDashboardPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+        testUtility = new TestUtility(driver);
+    }
+
+    @FindBy(css = "li[class=\"current\"]>a strong")
+    WebElement myOrdersLink;
+
+
+
+    public void clickOnMyOrdersLink() {
+        testUtility.waitForElementPresent(myOrdersLink);
+        myOrdersLink.click();
+    }
+
+
+
+
 }
+
