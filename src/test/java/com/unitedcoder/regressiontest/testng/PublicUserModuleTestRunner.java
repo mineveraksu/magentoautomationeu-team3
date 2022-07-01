722c9829f1f7f87a04a6c65360f4a4e5e1da24d3
hnur <gunes8811@hotmail.com>
2022-07-01 11:35:20 +0200

    public and customer module

--- src/test/java/com/unitedcoder/regressiontest/testng/PublicUserModuleTestRunner.java
+++ src/test/java/com/unitedcoder/regressiontest/testng/PublicUserModuleTestRunner.java
@@ -3,15 +3,14 @@ package com.unitedcoder.regressiontest.testng;
 import com.seleniummaster.maganto.frontendpages.AccountInformationPage;
 import com.seleniummaster.maganto.frontendpages.LoginPage;
 import com.seleniummaster.maganto.frontendpages.MyDashboardPage;
+import com.seleniummaster.maganto.frontendpages.MyOrdersPage;
 import com.seleniummaster.maganto.utility.ApplicationConfig;
 import com.seleniummaster.maganto.utility.BasePage;
 import com.seleniummaster.maganto.utility.TestResultListener;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.ITestContext;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Listeners;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 @Listeners(TestResultListener.class)
 public class PublicUserModuleTestRunner extends BasePage {
@@ -20,6 +19,7 @@ public class PublicUserModuleTestRunner extends BasePage {
     String config = "config.properties";
     MyDashboardPage dashboardPage;
     AccountInformationPage accountInformationPage;
+    MyOrdersPage myOrdersPage;
 
     @BeforeClass
     public void setup(ITestContext context){
@@ -29,6 +29,7 @@ public class PublicUserModuleTestRunner extends BasePage {
         loginPage.login();
         dashboardPage=new MyDashboardPage(driver);
         accountInformationPage=new AccountInformationPage(driver);
+        myOrdersPage=new MyOrdersPage(driver);
     }
 
     @Test(description ="EditAccountInformation")
@@ -37,6 +38,13 @@ public class PublicUserModuleTestRunner extends BasePage {
         dashboardPage.clickOnAccountInformationLink();
         accountInformationPage.editAccountInformation();
         Assert.assertTrue(accountInformationPage.verifyEditAccountInformation());
+    }
+
+    @Test(description = "A User Should be Able to View his/her Orders")
+    public void viewOrders(){
+        dashboardPage.clickOnMyOrdersLink();
+        Assert.assertTrue(myOrdersPage.viewOrders());
+
     }
 
 
