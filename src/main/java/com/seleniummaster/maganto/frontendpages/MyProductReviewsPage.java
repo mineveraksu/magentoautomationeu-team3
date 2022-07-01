90d004d49d7653ed4f95b1cbcceebcf4b0bb00a6
Shatgul.Pakzat <s.sawur@gmail.com>
2022-07-01 09:21:02 +0300

    Public module test case 12-Shatgul

--- src/main/java/com/seleniummaster/maganto/frontendpages/MyProductReviewsPage.java
+++ src/main/java/com/seleniummaster/maganto/frontendpages/MyProductReviewsPage.java
@@ -21,25 +21,25 @@ public class MyProductReviewsPage {
     @FindBy(linkText = "Cart")
     WebElement cartLink;
 
-    @FindBy(xpath = "//*[@id=\"cart-sidebar\"]/li[1]/div/a[1]")
+    @FindBy(linkText = "Edit item")
     WebElement editItemLink;
 
     @FindBy(linkText = "Add Your Review")
     WebElement addYourReviewLink;
 
-    @FindBy(id="Quality_3")
+    @FindBy(id = "Quality_3")
     WebElement qualityRadio;
 
-    @FindBy(id="Price_3")
+    @FindBy(id = "Price_3")
     WebElement priceRadio;
 
-    @FindBy(id="Value_3")
+    @FindBy(id = "Value_3")
     WebElement valueRadio;
 
-    @FindBy(id="review_field")
+    @FindBy(id = "review_field")
     WebElement reviewField;
 
-    @FindBy(id="summary_field")
+    @FindBy(id = "summary_field")
     WebElement summaryField;
 
     @FindBy(linkText = "Submit Review")
@@ -48,8 +48,14 @@ public class MyProductReviewsPage {
     @FindBy(linkText = "My Product Reviews")
     WebElement myProductReviewsLink;
 
+    @FindBy(linkText = "Account")
+    WebElement accountLink;
 
-    public void openAddReviewsPage(){
+    @FindBy(linkText = "My Account")
+    WebElement myAccountLink;
+
+
+    public void openAddReviewsPage() {
         testUtility.waitForElementPresent(cartLink);
         cartLink.click();
         testUtility.waitForElementPresent(editItemLink);
@@ -58,55 +64,42 @@ public class MyProductReviewsPage {
         addYourReviewLink.click();
     }
 
-    public void addReview(){
+    public void addReview() {
         testUtility.waitForElementPresent(qualityRadio);
         qualityRadio.click();
         priceRadio.click();
         valueRadio.click();
         reviewField.sendKeys(ApplicationConfig
-                .readFromConfigProperties(config,"myreviewcontent"));
+                .readFromConfigProperties(config, "myreviewcontent"));
         summaryField.sendKeys(ApplicationConfig.
-                readFromConfigProperties(config,"myreviewcontent"));
+                readFromConfigProperties(config, "myreviewcontent"));
         submitReviewLink.click();
     }
 
-    public void clickOnMyProductLink(){
+    public void clickOnMyProductLink() {
+        testUtility.waitForElementPresent(accountLink);
+        accountLink.click();
+        testUtility.waitForElementPresent(myAccountLink);
+        myAccountLink.click();
         testUtility.waitForElementPresent(myProductReviewsLink);
         myProductReviewsLink.click();
     }
 
-    public void verifyMyProductReviewsLinkDisplay(){
-        if (myProductReviewsLink.isDisplayed()){
+    public void verifyMyProductReviewsLinkDisplay() {
+        if (myProductReviewsLink.isDisplayed()) {
             System.out.println("MY PRODUCT REVIEWS link displayed successfully.");
-        }else {
+        } else {
             System.out.println("MY PRODUCT REVIEWS link can not be displayed .");
         }
     }
 
-    public void verifyMyProductReviewsContentsDisplayed(){
-        if(driver.getPageSource().contains
+    public void verifyMyProductReviewsContentsDisplayed() {
+        if (driver.getPageSource().contains
                 (ApplicationConfig.readFromConfigProperties
-                (config,"myreviewcontent"))){
+                        (config, "myreviewcontent"))) {
             System.out.println("Product reviews contents displayed.");
-        }else {
+        } else {
             System.out.println("Product reviews contents can not be displayed.");
         }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
