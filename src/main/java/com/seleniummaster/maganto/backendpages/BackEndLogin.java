ffd8fd17d09287268a2345bca2808cc074a7bed3
Sefiye <ayciver2019@gmail.com>
2022-07-02 21:40:06 +0300

    Backend Login Page Customer Module Test Runner Class - Sefiye

--- src/main/java/com/seleniummaster/maganto/backendpages/BackEndLogin.java
+++ src/main/java/com/seleniummaster/maganto/backendpages/BackEndLogin.java
@@ -1,4 +1,61 @@
 package com.seleniummaster.maganto.backendpages;
 
+import com.seleniummaster.maganto.utility.ApplicationConfig;
+import com.seleniummaster.maganto.utility.TestUtility;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class BackEndLogin {
+    WebDriver driver;
+    TestUtility testUtility;
+    String configFile = "config.properties";
+    String password = ApplicationConfig.readFromConfigProperties(configFile, "password");
+
+    public BackEndLogin(WebDriver driver) {
+        this.driver = driver;
+        testUtility = new TestUtility(driver);
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(name = "login[username]")
+    WebElement userNameField;
+    @FindBy(name = "login[password]")
+    WebElement passwordField;
+    @FindBy(css = ".form-button")
+    WebElement loginButton;
+    @FindBy(css = ".link-logout")
+    WebElement logoutButton;
+
+    public void customerPageLogin() {
+        testUtility.waitForElementPresent(userNameField);
+        userNameField.sendKeys(ApplicationConfig.readFromConfigProperties(configFile, "cususername"));
+        testUtility.waitForElementPresent(passwordField);
+        passwordField.sendKeys(password);
+        testUtility.waitForElementPresent(loginButton);
+        loginButton.click();
+    }
+
+    public void catalogPageLogin() {
+        testUtility.waitForElementPresent(userNameField);
+        userNameField.sendKeys(ApplicationConfig.readFromConfigProperties(configFile, "catusername"));
+        testUtility.waitForElementPresent(passwordField);
+        passwordField.sendKeys(password);
+        testUtility.waitForElementPresent(loginButton);
+        loginButton.click();
+    }
+
+    public boolean VerifyBackendLogin() {
+        testUtility.waitForElementPresent(logoutButton);
+        if (loginButton.isDisplayed()) {
+            System.out.println("Backend Login Successfully");
+            return true;
+        } else {
+            System.out.println("Backend Login failed");
+            return false;
+        }
+    }
+
+
 }
--- src/test/java/com/unitedcoder/regressiontest/testng/CustomerModuleTestRunner.java
+++ src/test/java/com/unitedcoder/regressiontest/testng/CustomerModuleTestRunner.java
@@ -1,4 +1,38 @@
 package com.unitedcoder.regressiontest.testng;
 
-public class CustomerModuleTestRunner {
+import com.seleniummaster.maganto.backendpages.BackEndLogin;
+import com.seleniummaster.maganto.backendpages.customerpages.CustomerDashboardPage;
+import com.seleniummaster.maganto.utility.ApplicationConfig;
+import com.seleniummaster.maganto.utility.BasePage;
+import com.seleniummaster.maganto.utility.BrowserType;
+import org.testng.ITestContext;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class CustomerModuleTestRunner extends BasePage {
+    final String configFile = "config.properties";
+    BackEndLogin login;
+    CustomerDashboardPage customerDashboardPage;
+
+    @BeforeClass
+    public void setup(ITestContext context){
+        String url= ApplicationConfig.readFromConfigProperties(configFile,"url");
+        browserSetUp(url, BrowserType.CHROME);
+        context.setAttribute("driver",driver);
+        login=new BackEndLogin(driver);
+        login.customerPageLogin();
+        customerDashboardPage=new CustomerDashboardPage(driver);
+    }
+
+
+
+
+
+
+
+    @AfterClass
+    public void tearDown(){
+        closeBrowser();
+    }
 }
