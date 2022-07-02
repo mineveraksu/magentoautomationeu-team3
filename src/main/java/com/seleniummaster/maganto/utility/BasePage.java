b6978a809d9585dc92c8117dcc918fd9926d2210
Sefiye <ayciver2019@gmail.com>
2022-07-02 12:09:43 +0300

    Base page new setup  - Sefiye

--- src/main/java/com/seleniummaster/maganto/utility/BasePage.java
+++ src/main/java/com/seleniummaster/maganto/utility/BasePage.java
@@ -5,37 +5,45 @@ import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.safari.SafariOptions;
 
 public class BasePage {
-    public  WebDriver driver;
-    public  String browserName = "chrome";
+    public WebDriver driver;
 
-    public  void browserSetUp(String Url) {
-        if (driver == null) {
-            if (browserName.equalsIgnoreCase("chrome")) {
-
-                System.setProperty("webdriver.chrome.driver", "c:\\webdriver\\chromedriver.exe");
-                driver = new ChromeDriver();
-                driver.manage().window().maximize();
-                driver.get(Url);
-
-            } else if (browserName.equalsIgnoreCase("Fire fox")) {
-                System.setProperty("webdriver.gecko.driver", "c:\\webdriver\\geckodriver.exe");
-                driver = new FirefoxDriver();
-                driver.manage().window().maximize();
-                driver.get(Url);
-            }
-            else if (browserName.equalsIgnoreCase("safari")) {
-                System.setProperty("webdriver.safari.driver", "c:\\safaridriver.exe");
-                driver = new FirefoxDriver();
-                driver.manage().window().maximize();
-                driver.get(Url);
-            }
+    public void browserSetUp(String url, BrowserType browserType) {
+        switch (browserType) {
+            case CHROME:
+                ChromeOptions chromeOptions = new ChromeOptions();
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver(chromeOptions);
+                break;
+            case FIREFOX:
+                WebDriverManager.firefoxdriver().setup();
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                driver = new FirefoxDriver(firefoxOptions);
+                break;
+            case SAFARI:
+                WebDriverManager.safaridriver().setup();
+                SafariOptions safariOptions = new SafariOptions();
+                driver = new SafariDriver(safariOptions);
+                break;
+            case EDGE:
+                WebDriverManager.edgedriver().setup();
+                EdgeOptions edgeOptions = new EdgeOptions();
+                driver = new EdgeDriver(edgeOptions);
+                break;
         }
+        driver.manage().window().maximize();
+        driver.get(url);
     }
 
-    public  void closeBrowser() {
+    public void closeBrowser() {
         driver.quit();
     }
 }
+
