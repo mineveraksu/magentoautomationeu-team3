57d66bb44e47a4b295793dd61e2511a436383e46
Shatgul.Pakzat <s.sawur@gmail.com>
2022-07-20 09:58:10 +0300

    bosh

--- src/main/java/com/seleniummaster/maganto/backendpages/customerpages/FilterCustomerPage.java
+++ src/main/java/com/seleniummaster/maganto/backendpages/customerpages/FilterCustomerPage.java
@@ -9,8 +9,6 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 
-
-
 public class FilterCustomerPage {
     WebDriver driver;
     TestUtility testUtility;
