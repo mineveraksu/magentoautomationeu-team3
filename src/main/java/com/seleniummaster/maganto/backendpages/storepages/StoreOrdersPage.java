dc63d59881a5a241ae53d91ddac7849c5ddbac53
tursunay <trsnyy@yahoo.com>
2022-07-24 17:30:39 +0300

    store module edit order test(final)

--- src/main/java/com/seleniummaster/maganto/backendpages/storepages/StoreOrdersPage.java
+++ src/main/java/com/seleniummaster/maganto/backendpages/storepages/StoreOrdersPage.java
@@ -1,8 +1,10 @@
 package com.seleniummaster.maganto.backendpages.storepages;
 
 import com.seleniummaster.maganto.utility.TestUtility;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
@@ -10,25 +12,19 @@ public class StoreOrdersPage {
 
     WebDriver driver;
     TestUtility testUtility;
+    Actions actions;
 
-    @FindBy(xpath = "//span[text()='Orders']")
-    WebElement ordersLink;
-    @FindBy(css="#sales_order_grid_filter_real_order_id")
-    WebElement orderIdField;
-    @FindBy(xpath = "//span[(text()='Search')]")
-    WebElement searchButton;
-    @FindBy(xpath = "(//a[text()='View'])[1]")
-    WebElement viewLink;
-    @FindBy(xpath = "(//span[text()=\"Edit\"])[1]")
+
+    @FindBy(xpath ="(//span[text()=\"Edit\"])[1]")
     WebElement editButton;
     @FindBy(xpath = "//input[@id=\"order-billing_address_postcode\"]")
     WebElement zipCodeField;
     @FindBy(xpath = "//input[@id=\"order-billing_address_telephone\"]")
     WebElement telephoneField;
-    @FindBy(xpath = "//input[@id=\"p_method_checkmo\"]")
-    WebElement checkOrderButton;
-    @FindBy(xpath = "Shipping & Handling Information")
-    WebElement shippingAndHandlingInformationField;
+    @FindBy(xpath ="//div[@class=\"order-totals-bottom\"]//p[3]//button//span[text()=\"Submit Order\"]")
+    WebElement submitOrderButton;//cancel
+    @FindBy(xpath ="//span[text()='The order has been created.']")
+    WebElement verifyOrderSuccessfulEdited;
 
 
     //Store Manager can edit orders
@@ -38,6 +34,36 @@ public class StoreOrdersPage {
         this.driver = driver;
         PageFactory.initElements(driver,this);
         testUtility=new TestUtility(driver);
+        actions=new Actions(driver);
+    }
+
+    public void editOrderInformation(){
+        testUtility.waitForElementPresent(editButton);
+        actions.moveToElement(editButton).click().perform();
+        Alert alert=driver.switchTo().alert();
+        alert.accept();
+        testUtility.waitForElementPresent(zipCodeField);
+        zipCodeField.clear();
+        zipCodeField.sendKeys(testUtility.generateZipCode());
+        testUtility.waitForElementPresent(telephoneField);
+        telephoneField.clear();
+        telephoneField.sendKeys(testUtility.generateTelephoneNumber());
+        testUtility.sleep(2);
+        //testUtility.waitForElementPresent(submitOrderButton);
+        actions.moveToElement(submitOrderButton).click().perform();
+        System.out.println(" submit button clicked ");
+        testUtility.sleep(3);
+    }
+
+    public boolean verifyOrderEdited(){
+        testUtility.waitForElementPresent(verifyOrderSuccessfulEdited);
+        if(verifyOrderSuccessfulEdited.getText().contains("created.")){
+            System.out.println(" Store manager edit order successful !");
+            return true;
+        }else{
+            System.out.println("Store manager edit order failed ! ");
+            return false;
+        }
     }
 
 
