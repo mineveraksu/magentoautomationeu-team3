a863c6a066219e47a915a7600a0aaf721e81f0e4
Sefiye <ayciver2019@gmail.com>
2022-08-04 13:28:38 +0300

    Merge branch 'master' into MAGE2022-5MAGE2022-567-verify-newly-added-customer-group-should-be-in-the-database

--- src/main/java/com/seleniummaster/maganto/database/DataAccess.java
+++ src/main/java/com/seleniummaster/maganto/database/DataAccess.java
@@ -8,6 +8,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DataAccess {
+    // getNewlyRegisteredUser Method
     public boolean getNewlyRegisteredUser(String userEmail, Connection connection){
         boolean isUserExist=false;
         Statement statement=null;
@@ -62,6 +63,63 @@ public class DataAccess {
             if(count>=1)
                 isUserExist=true;
             return isUserExist;
+        }
+    }
+    //getCustomerGroup Method
+    public boolean getCustomerGroup(String customerGroupName, Connection connection){
+        boolean isCustomerGroupExist=false;
+        Statement statement=null;
+        ResultSet resultSet=null;
+        CachedRowSet cachedRowSet=null;
+
+        try {
+            cachedRowSet= RowSetProvider.newFactory().createCachedRowSet();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            statement = connection.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        String customerGroupSqlScript=String.format("select customer_group_code,customer_group_id from mg_customer_group where customer_group_code='%s';",customerGroupName);
+        try {
+            resultSet=statement.executeQuery(customerGroupSqlScript);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        if(resultSet==null){
+            System.out.println("no records found");
+            return isCustomerGroupExist;
+        }else{
+            try {
+                cachedRowSet.populate(resultSet);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            int count=0;
+            while (true){
+                try {
+                    if(!cachedRowSet.next()){
+                        break;
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    int groupId = cachedRowSet.getInt("customer_group_id");
+                    String groupName = cachedRowSet.getString("customer_group_code");
+                    System.out.println(String.format("customer_group_id=%d customer_group_code=%s",groupId,groupName));
+                    count = cachedRowSet.getRow();
+                }catch(SQLException e){
+                    e.printStackTrace();
+                }
+            }
+            if(count>=1)
+                isCustomerGroupExist=true;
+            return isCustomerGroupExist;
         }
     }
 
