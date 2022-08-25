f146754d95742215fc1a2cea9cfc42aed634f693
hnur <nur.sdet@proton.me>
2022-08-25 09:27:27 +0200

    Catalog and store module

--- src/test/java/com/unitedcoder/regressiontest/testng/CatalogModuleTestRunner.java
+++ src/test/java/com/unitedcoder/regressiontest/testng/CatalogModuleTestRunner.java
@@ -6,7 +6,6 @@ import com.seleniummaster.maganto.database.ConnectionManager;
 import com.seleniummaster.maganto.database.DataAccess;
 import com.seleniummaster.maganto.utility.*;
 import org.testng.Assert;
-import org.testng.ITest;
 import org.testng.ITestContext;
 import org.testng.annotations.*;
 
@@ -144,13 +143,13 @@ public class CatalogModuleTestRunner extends BasePage {
         return data;
     }
 
-//    @DataProvider
-//    public Object[] newCategoryDescription() {
-//        Object[] data = new Object[]{
-//                TestUtility.getFieldFromJson("Test-Data/testDatasSmall.json", "new_category_description")
-//        };
-//        return data;
-//    }
+    @DataProvider
+    public Object[] newCategoryDescription() {
+        Object[] data = new Object[]{
+                TestUtility.getFieldFromJson("Test-Data/testDatasSmall.json", "new_category_description")
+        };
+        return data;
+    }
 
     @DataProvider
     public Object[] AttributeInfo() {
