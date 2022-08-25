0c553b37e8a755a67c314a05f1bf6464b46e87e9
Matros <guzalhayat84@gmail.com>
2022-08-25 21:41:19 +0300

    api test update customer finish all hu

--- src/main/java/com/seleniummaster/maganto/api/CustomerPayload.java
+++ src/main/java/com/seleniummaster/maganto/api/CustomerPayload.java
@@ -14,11 +14,11 @@ public class CustomerPayload {
     @JsonProperty("value")
     private String value;
 
-    public CustomerPayload(int attribute_id, int entity_id, int entity_type_id, int id, String value) {
+    public CustomerPayload(int attribute_id, int entity_id,int entity_type_id, int id, String value) {
         this.attribute_id = attribute_id;
         this.entity_id = entity_id;
         this.entity_type_id = entity_type_id;
         this.id = id;
         this.value = value;
-    }
+    }//api iqin
 }
