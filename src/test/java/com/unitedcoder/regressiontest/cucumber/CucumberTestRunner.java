22a23cc68f008d9d1b8a7a011d05590efbdd0366
Sefiye <ayciver2019@gmail.com>
2022-09-04 19:34:58 +0300

    Merge branch 'master' into MAGE2022-606-user-should-be-able-to-create-and-update-a-customer-group-using-api--sefiye

--- src/test/java/com/unitedcoder/regressiontest/cucumber/CucumberTestRunner.java
+++ src/test/java/com/unitedcoder/regressiontest/cucumber/CucumberTestRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
                 "json:target/cucumber.json",
                 "junit:target/cucumber-results.xml"},//for test report
         features = {"src/test/resources"},//feature file location
-        tags ="@RegressionTest or @ApiTest or @DatabaseTest")
+        tags ="@RegressionTest and @ApiTest and @DatabaseTest")
 
 
 public class CucumberTestRunner {
